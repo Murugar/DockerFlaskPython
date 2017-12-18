c01826e71f79dbd3b0ce200eb14d2f68336508bf
Davanon <davanon2014@gmail.com>
2017-12-18 15:32:54 +0530

    First

---
+++ project/__init__.py
@@ -0,0 +1,20 @@
+# project/__init__.py
+import os
+from flask import Flask, jsonify
+
+
+# instantiate the app
+app = Flask(__name__)
+
+
+# set app config
+app_settings=os.getenv('APP_SETTINGS')
+app.config.from_object(app_settings)
+
+
+@app.route('/test', methods=['GET'])
+def ping_pong():
+    return jsonify({
+        'status': 'success',
+        'message': 'flask docker test'
+    })
